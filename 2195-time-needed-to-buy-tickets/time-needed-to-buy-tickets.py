87dba1c60b3e98bcc5988624ebb0080d1d11bbdf
Harsh Sharma <104566605+harshpanzer@users.noreply.github.com>
2024-04-09 22:19:51 +0530

    Time: 34 ms (94.18%) | Memory: 16.6 MB (51.83%) - LeetSync

--- 2195-time-needed-to-buy-tickets/time-needed-to-buy-tickets.py
+++ 2195-time-needed-to-buy-tickets/time-needed-to-buy-tickets.py
@@ -5,11 +5,17 @@ class Solution:
         ans=0
         for i in range(length):
             if(tickets[i]<=a and i<k):
+                ans+=tickets[i]
                 continue
             elif(i>k and tickets[i]>=a):
                 tickets[i]=a-1
+                ans+=tickets[i]
             elif(tickets[i]>a and i<k):
                 tickets[i]=a
-        for i in range(length):
-            ans+=tickets[i]
+                ans+=tickets[i]
+            else:
+                ans+=tickets[i]
+            
+        # for i in range(length):
+        #     ans+=tickets[i]
         return ans
