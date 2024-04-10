14276987e9763adb8cccb3d65fa02cdcd09e8669
Harsh Sharma <104566605+harshpanzer@users.noreply.github.com>
2024-04-10 18:14:12 +0530

    Time: 42 ms (80.14%) | Memory: 16.6 MB (97.97%) - LeetSync

---
+++ 987-reveal-cards-in-increasing-order/reveal-cards-in-increasing-order.py
@@ -0,0 +1,27 @@
+def indexx(sel,length):
+    if(sel<length):
+        return sel
+    while(sel>length-1):
+        sel=sel%length
+        sel=(sel*2)+1
+        return indexx(sel,length)
+
+class Solution:
+
+    def deckRevealedIncreasing(self, deck: List[int]) -> List[int]:
+        
+        deck.sort()
+        length=len(deck)
+        ans=[]
+        for i in range(length):
+            ans.append(0)
+        for i in range(length):
+            sel=i*2
+            if(sel<=length-1):
+                ans[sel]=deck[i]
+                
+            elif(sel>length-1):
+                
+                a=indexx(sel,length)
+                ans[a]=deck[i]
+        return ans
