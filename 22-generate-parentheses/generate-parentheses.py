b559c4975d5779d96e68378ebd4e5eb8f57ee237
Harsh Sharma <104566605+harshpanzer@users.noreply.github.com>
2024-06-26 19:27:17 +0530

    Time: 41 ms (33.15%) | Memory: 16.9 MB (11.27%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.py
+++ 22-generate-parentheses/generate-parentheses.py
@@ -1,15 +1,26 @@
 class Solution(object):
     def generateParenthesis(self, n):
-        def backtrack(S='', left=0, right=0):
-            if len(S) == 2 * n:
-                result.append(S)
+        # def backtrack(S='', left=0, right=0):
+        #     if len(S) == 2 * n:
+        #         result.append(S)
+        #         return
+        #     if left < n:
+        #         backtrack(S + '(', left + 1, right)
+        #     if right < left:
+        #         backtrack(S + ')', left, right + 1)
+        #     print(S)
+        # result = []
+        # backtrack()
+        # return result
+        ans=[]
+        def backtrack(st="",left=0,right=0):
+
+            if(len(st)==2*n):
+                ans.append(st)
                 return
-            if left < n:
-                backtrack(S + '(', left + 1, right)
-            if right < left:
-                backtrack(S + ')', left, right + 1)
-            print(S)
-            return
-        result = []
+            if(left<n):
+                backtrack(st+"(",left+1,right)
+            if(right<left):
+                backtrack(st+")",left,right+1)
         backtrack()
-        return result
+        return ans
