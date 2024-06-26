5dc18294d5d30b3feef0f60a184314685df73a75
Harsh Sharma <104566605+harshpanzer@users.noreply.github.com>
2024-06-26 20:48:52 +0530

    Time: 40 ms (34.73%) | Memory: 16.9 MB (6.94%) - LeetSync

--- 78-subsets/subsets.py
+++ 78-subsets/subsets.py
@@ -1,7 +1,16 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        ans=[[]]
-        for i in nums:
-            for j in range (len(ans)):
-                ans+=[[i]+ans[j]]
-        return ans
+        res=[]
+        sol=[]
+        length=len(nums)
+        def gen(i):
+            if(i==length):
+                print(sol)
+                res.append(sol[:])
+                return
+            gen(i+1)
+            sol.append(nums[i])
+            gen(i+1)
+            sol.pop()
+        gen(0)
+        return res
