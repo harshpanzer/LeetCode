52b1b25d001e5a853746a51b2dd9b66205753d29
Harsh Sharma <104566605+harshpanzer@users.noreply.github.com>
2024-09-15 00:44:21 +0530

    Time: 40 ms (23.47%) | Memory: 16.6 MB (15.50%) - LeetSync

--- 62-unique-paths/unique-paths.py
+++ 62-unique-paths/unique-paths.py
@@ -1,6 +1,7 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        @cache
+        ans=[[-1 for _ in range(n)] for _ in range(m)]
+        print(ans)
         def sol(v,h):
             a=0
             b=0
@@ -8,12 +9,13 @@ class Solution:
                 return 0
             if(v==m-1 and h==n-1):
                 return 1
-
+            if ans[v][h] != -1:
+                return ans[v][h]
             if(v<m-1):
                 a=sol(v+1,h)
             if(h<n-1):
                 b=sol(v,h+1)
             print(a+b)
-            
-            return a+b
+            ans[v][h]= a+b
+            return ans[v][h]
         return sol(0,0)
