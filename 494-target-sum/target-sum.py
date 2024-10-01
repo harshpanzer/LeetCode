cc3c8fd783de58837bf2d8a594d78f4ed1d96fb5
Harsh Sharma <104566605+harshpanzer@users.noreply.github.com>
2024-10-01 15:08:36 +0530

    Time: 194 ms (64.02%) | Memory: 32.5 MB (52.92%) - LeetSync

---
+++ 494-target-sum/target-sum.py
@@ -0,0 +1,20 @@
+class Solution:
+    def findTargetSumWays(self, nums: List[int], target: int) -> int:
+        length=len(nums)
+        dic={}
+        def sol(i,temp):
+            if(i==length-1):
+                if((temp-nums[i])==target and (temp+nums[i])==target):
+
+                    return 2
+                elif((temp-nums[i])==target or (temp+nums[i])==target):
+                    return 1
+                else:
+                    return 0
+            if(dic.get((i,temp))!=None):
+                return dic.get((i,temp))
+            a=sol(i+1,temp+nums[i])
+            b=sol(i+1,temp-nums[i])
+            dic[(i,temp)]=(a+b)
+            return a+b
+        return sol(0,0)
